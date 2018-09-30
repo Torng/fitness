1d10d21c464511000d285177de8307af2b3425ad
Torng <cool870427@gmail.com>
2018-09-30 16:03:06 +0800

    Initial Commit

---
+++ fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitness
+//
+//  Created by 童俞豪 on 2018/9/30.
+//  Copyright © 2018年 童俞豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
